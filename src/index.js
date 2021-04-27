27efec7b7fcf2297f206429ee67ee68bef6b28c6
woalstm85@naver.com <woalstm85@naver.com>
2021-04-27 10:33:06 +0900

    qq\

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import '@fortawesome/fontawesome-free/js/all.js';
-
-import App from './app';
+import SimpleHabit from './components/simpleHabit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SimpleHabit />
   </React.StrictMode>,
   document.getElementById('root')
 );
